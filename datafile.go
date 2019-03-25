b0878b6e794e386b6fc37fdaad658c0e3a9fc1a0
HelloChenHZ <chenhangzhong88@gmail.com>
2019-03-25 22:55:21 +0800

    add datafile struct

--- datafile.go
+++ datafile.go
@@ -1,6 +1,9 @@
 package nutsdb
 
-import "github.com/pkg/errors"
+import (
+	"encoding/binary"
+	"errors"
+)
 
 var (
 	// ErrCrcZero is returned when crc is 0
@@ -45,7 +48,7 @@ func NewDataFile(path string, capacity int64, rwMode RWMode) (df *DataFile, err 
 		}
 	}
 
-	if rwMode = MMap {
+	if rwMode == MMap {
 		rwManager, err = NewMMapRWManager(path, capacity)
 		if err != nil {
 			return nil, err
@@ -58,4 +61,97 @@ func NewDataFile(path string, capacity int64, rwMode RWMode) (df *DataFile, err 
 		ActualSize: 0,
 		rwManager: rwManager,
 	}, nil
+}
+
+// ReadAt returns entry at the given off(offset)
+func (df *DataFile) ReadAt(off int) (e *Entry, err error) {
+	buf := make([]byte, DataEntryHeaderSize)
+
+	if _, err := df.rwManager.ReadAt(buf, int64(off)); err != nil {
+		return nil, err
+	}
+
+	meta := readMetaData(buf)
+
+	e = &Entry {
+		crc:	binary.LittleEndian.Uint32(buf[0:4]),
+		Meta:	meta
+	}
+
+	if e.IsZero() {
+		return nil, nil
+	}
+
+	// read bucket
+	off += DataEntryHeaderSize
+	bucketBuf := make([]byte, meta.bucketSize)
+	_, err = df.rwManager.ReadAt(bucketBuf, int64(off))
+	if err != nil {
+		return nil, err
+	}
+
+	e.Meta.bucket = bucketBuf
+
+	// read key
+	off += int(meta.bucketSize)
+	keyBuf := make([]byte, meta.keySize)
+
+	_, err = df.rwManager.ReadAt(keyBuf, int64(off))
+	if err != nil {
+		return nil, err
+	}
+	e.Value = keyBuf
+
+	// read value
+	off += int(meta.keySize)
+	valBuf := make([]byte, meta.valueSize)
+	_, err = df.rwManager.ReadAt(valBuf, int64(off))
+	if err != nil {
+		return nil, err
+	}
+	e.Value = valBuf
+
+	crc := e.GetCrc(buf)
+	if crc != e.crc {
+		return nil, ErrCrc
+	}
+
+	return
+}
+
+// WriteAt copies data to mapped region from the b slice starting at
+// given off and returns number of bytes copied to the mapped region
+func (df *DataFile) WriteAt(b []byte, off int64) (n int, err error) {
+	return df.rwManager.WriteAt(b, off)
+}
+
+// Sync commits the current contents of the file to stable storage
+// Typically, this meads flushing the file system's in-memory copy
+// of recently written data to disk
+func (df *DataFile) Sync() (err error) {
+	return df.rwManager.Sync()
+}
+
+// Close closes the RWManager
+// If RWManager is FileManager represents close the file
+// rendering it unusable for I/O
+// If RWManager is a MMapRWManager represents Unmap deletes the memory mapped region
+// flushes any remaining changes
+func (df *DataFile) Close() (err error) {
+	return df.rwManager.Close()
+}
+
+// readMetaData returns MetaData at given buf slice
+func readMetaData(buf []byte) *MetaData {
+	return &MetaData {
+		timestamp:	binary.LittleEndian.Uint64(buf[4:12]),
+		keySize: 	binary.LittleEndian.Uint32(buf[12:16]),
+		valueSize:	binary.LittleEndian.Uint32(buf[16:20]),
+		Flag:		binary.LittleEndian.Uint16(buf[20:22]),
+		TTL:		binary.LittleEndian.Uint32(buf[22:26]),
+		bucketSize:	binary.LittleEndian.Uint32(buf[26:30]),
+		status:		binary.LittleEndian.Uint16(buf[30:32]),
+		ds:			binary.LittleEndian.Uint16(buf[32:34]),
+		txID:		binary.LittleEndian.Uint64(buf[34,42]),
+	}
 }
--- entry.go
+++ entry.go
@@ -1,5 +1,10 @@
 package nutsdb
 
+import (
+	"encoding/binary"
+	"hash/crc32"
+)
+
 type (
 	//Entry represents the data item
 	Entry struct {
@@ -39,3 +44,63 @@ func (e *Entry) Size() int64 {
 }
 
 // Encode returns the slice after the entry be encoded
+//
+//  the entry stored format:
+//  |----------------------------------------------------------------------------------------------------------------|
+//  |  crc  | timestamp | ksz | valueSize | flag  | TTL  |bucketSize| status | ds   | txId |  bucket |  key  | value |
+//  |----------------------------------------------------------------------------------------------------------------|
+//  | uint32| uint64  |uint32 |  uint32 | uint16  | uint32| uint32 | uint16 | uint16 |uint64 |[]byte|[]byte | []byte |
+//  |----------------------------------------------------------------------------------------------------------------|
+//
+func (e *Entry) Encode() []byte {
+	keySize := e.Meta.keySize
+	valueSize := e.Meta.valueSize
+	bucketSize := e.Meta.bucketSize
+
+	// set DataItemHeader buf
+	buf := make([]byte, e.Size())
+	buf = e.setEntryHeaderBuf(buf)
+	// set bucket\key\value
+	copy(buf[DataEntryHeaderSize:(DataEntryHeaderSize+bucketSize)], e.Meta.bucket)
+	copy(buf[DataEntryHeaderSize+bucketSize:(DataEntryHeaderSize+bucketSize+keySize)], e.Key)
+	copy(buf[(DataEntryHeaderSize+bucketSize+keySize):(DataEntryHeaderSize+bucketSize+keySize+valueSize)], e.Value)
+
+	c32 := crc32.ChecksumIEEE(buf[4:])
+	binary.LittleEndian.PutUint32(buf[0:4], c32)
+
+	return buf
+}
+
+// setEntryHeaderBuf sets the entry header buff.
+func (e *Entry) setEntryHeaderBuf(buf []byte) []byte {
+	binary.LittleEndian.PutUint64(buf[4:12], e.Meta.timestamp)
+	binary.LittleEndian.PutUint32(buf[12:16], e.Meta.keySize)
+	binary.LittleEndian.PutUint32(buf[16:20], e.Meta.valueSize)
+	binary.LittleEndian.PutUint16(buf[20:22], e.Meta.Flag)
+	binary.LittleEndian.PutUint32(buf[22:26], e.Meta.TTL)
+	binary.LittleEndian.PutUint32(buf[26:30], e.Meta.bucketSize)
+	binary.LittleEndian.PutUint16(buf[30:32], e.Meta.status)
+	binary.LittleEndian.PutUint16(buf[32:34], e.Meta.ds)
+	binary.LittleEndian.PutUint64(buf[34:42], e.Meta.txID)
+
+	return buf
+}
+
+// IsZero checks if the entry is zero or not
+func (e *Entry) IsZero() bool {
+	if e.crc == 0 && e.Meta.keySize == 0 && e.Meta.valueSize == 0 && e.Meta.timestamp == 0 {
+		return true
+	}
+
+	return false
+}
+
+// GetCrc returns the crc at given buf slice
+func (e *Entry) GetCrc(buf []byte) uint32 {
+	crc := crc32.ChecksumIEEE(buf[4:])
+	crc = crc32.Update(crc, crc32.IEEETable, e.Meta.bucket)
+	crc = crc32.Update(crc, crc32.IEEETable, e.Key)
+	crc = crc32.Update(crc, crc32.IEEETable, e.Value)
+
+	return crc
+}
